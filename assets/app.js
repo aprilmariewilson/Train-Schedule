71937c6bcaad2344c19d83ecf9654c1af54da8aa
April Wilson <aprilmariewilson@yahoo.com>
2018-03-07 23:39:39 -0700

    fixed most js still a couple undefined errors

--- assets/app.js
+++ assets/app.js
@@ -38,7 +38,7 @@ $(function(){
 			frequency = $(".frequency")
 				.val()
 				.trim();
-				var newTrain = {name:name, destination:destination,firstTrain:firstTrain, frequency:frequency}
+				var newTrain = {name:name, destination:destination, firstTrain:firstTrain, frequency:frequency}
 				database.ref().push(newTrain);
 	
 			console.log(newTrain);
@@ -46,16 +46,43 @@ $(function(){
 		});
 		database.ref().on('child_added', function(snapShot) {
 			var data = snapShot.val();
-			var formattedTime = moment(data.firstTrain).format('HHmm');
-			var minutesAway = moment().diff(moment(formattedTime), 'minutes');
+
+			var firstTrain = $('.time').val().trim();
+			console.log(firstTrain)
+
+			var tFrequency = $('.frequency').val().trim();
+
+			var tRemainder = diffTime % tFrequency;
+		console.log(tRemainder);
+			
+			var firstTrainConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
+		console.log(firstTrainConverted);
+
+			var currentTime = moment();
+			console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+			var diffTime = moment().diff(moment(firstTrainConverted), "minutes");
+		console.log("DIFFERENCE IN TIME: " + diffTime);
+
+			var minutesAway = moment().diff(moment(currentTime), 'minutes');
 			console.log('child added', snapShot.val());
+
+			var diffTime = moment().diff(moment(firstTrainConverted), "minutes");
+		console.log("DIFFERENCE IN TIME: " + diffTime);
+
+		var tMinutesTillTrain = tFrequency - tRemainder;
+		console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+		var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+		console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
 			var tbody = $('#currentTrain > tbody');
 			var tr = $('<tr>');
 			var tdName = $('<td>').text(data.name);
 			var tdDestination = $('<td>').text(data.destination);
-			var tdFrequency = $('<td>').text(formattedtime);
-			var tdArrival = $('<td>').text(firstTrain + frequency);
-			var tdMinutesAway = $('<td>').text(frequency - data.nextArrival);
+			var tdFrequency = $('<td>').text(currentTime);
+			var tdArrival = $('<td>').text(tMinutesTillTrain);
+			var tdMinutesAway = $('<td>').text(tMinutesTillTrain);
 	
 	
 			$('<tr>').append(tdName, tdDestination, tdFrequency, tdArrival, tdMinutesAway);
